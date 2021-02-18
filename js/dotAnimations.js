9aa0e7c0c7095118639fccaa84eddeb0fbeed923
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-02-18 17:40:47 +0530

    Added some css

---
+++ js/dotAnimations.js
@@ -0,0 +1,78 @@
+const themeToDots = {
+  'BLACK' : 'black-dot',
+  'WHITE' : 'white-dot',
+  'DARK' : 'red-dot',
+  'GREEN' : 'green-dot',
+  'LIGHT' : 'light-dot'
+}
+
+const colors = {
+  BLACK  : '#000000',
+  WHITE : '#B6B6B6',
+  DARK : '#441921',
+  GREEN : '#007a2b',
+  LIGHT : '#c6ecd4'
+
+}
+
+
+function loadMode(mode){
+  const pageToLoad = './json/particlejs-config(' + mode + ').json';
+  localStorage.setItem('theme',mode);
+  currentTheme = mode;
+  document.body.style.backgroundColor = colors[mode];
+  document.getElementById('secondary-stylesheet').href=`./css/indexStyles(${mode}).css`
+
+
+  particlesJS.load('particles-js',pageToLoad,()=>{
+
+
+
+  });
+
+}
+let currentTheme = localStorage.getItem('theme');
+if (!currentTheme){
+  currentTheme = 'BLACK';
+}
+
+loadMode(currentTheme)
+
+
+
+document.getElementById("black-dot").addEventListener('click',(e)=>{
+  loadMode('BLACK');
+
+
+});
+
+document.getElementById("white-dot").addEventListener('click',(e)=>{
+  loadMode('WHITE');
+
+
+});
+
+document.getElementById("green-dot").addEventListener('click',(e)=>{
+  loadMode('GREEN');
+
+
+});
+
+
+
+
+document.getElementById("light-dot").addEventListener('click',(e)=>{
+  loadMode('LIGHT');
+
+
+});
+
+
+document.getElementById("red-dot").addEventListener('click',(e)=>{
+  loadMode('DARK');
+
+});
+
+
+
+
